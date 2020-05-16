49b32ff9870868298ccf0ee7b73a996b30eb73e4
Anush Salimov <safmail96@gmail.com>
2020-05-16 20:08:28 +0300

    specifying host

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,14 @@
-export default () => {
+import buildClient from "../api/build-client";
+
+const LandingPage = ({ currentUser }) => {
   return <h1>Main page</h1>;
 };
+
+LandingPage.getInitialProps = async context => {
+  const client = buildClient(context);
+  const { data } = client.get("/api/users/currentuser");
+
+  return data;
+};
+
+export default LandingPage;
