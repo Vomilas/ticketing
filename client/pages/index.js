7cd2c1e20bb04ea5d47106340c5c4c5033cc2848
Anush Salimov <safmail96@gmail.com>
2020-05-16 20:15:01 +0300

    add signin form

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,12 +1,16 @@
 import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
-  return <h1>Main page</h1>;
+  return currentUser ? (
+    <h1>You're signed in</h1>
+  ) : (
+    <h1>You're not signed in</h1>
+  );
 };
 
 LandingPage.getInitialProps = async context => {
   const client = buildClient(context);
-  const { data } = client.get("/api/users/currentuser");
+  const { data } = await client.get("/api/users/currentuser");
 
   return data;
 };
