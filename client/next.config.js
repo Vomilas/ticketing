6e9fb796e75e1a44f1f355d715b862ef5078ee2b
Anush Salimov <safmail96@gmail.com>
2020-06-01 23:48:08 +0300

    paying for an order

--- client/next.config.js
+++ client/next.config.js
@@ -2,5 +2,8 @@ module.exports = {
   webpackDevMiddleware: config => {
     config.watchOptions.poll = 300;
     return config;
+  },
+  env: {
+    stripePublicKey: "pk_test_4U1uvKQNmxX9FrcNiCpnaBxS"
   }
 };
