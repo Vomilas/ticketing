f5bf129a9e87f839ad4df842ed2ff30875f96d5e
Anush Salimov <safmail96@gmail.com>
2020-06-01 23:58:45 +0300

    list of orders

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -11,7 +11,7 @@ const OrderShow = ({ order, currentUser }) => {
     body: {
       orderId: order.id
     },
-    onSuccess: () => {}
+    onSuccess: () => Router.push("/orders")
   });
 
   useEffect(() => {
